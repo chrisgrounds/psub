1babe04b10f4346f3677e0991589cc8b4b95d321
Chris Bacon <chrisbacon2009@hotmail.com>
2019-11-20 20:43:00 +0000

    Fixed run example

--- run.js
+++ run.js
@@ -1,9 +1,9 @@
 const { Psub } = require("./Psub");
 
-const Psub = new Psub();
+const psub = new Psub();
 
-const gamesToken = Psub.subscribe("games", function(msg) {
+const gamesToken = psub.subscribe("games", function(msg) {
   console.log(msg);
 });
-Psub.publish("games", "red dead redemption");
+psub.publish("games", "red dead redemption");
 
